c22e4387b74ccd11ef5e335cabb43f5f78f4b9be
unknown <quoctung>
2019-03-02 13:28:13 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || 'tkshop.herokuapp.com/'
+const host = process.env.HOST || 'http://tkshop.herokuapp.com/'
 const port = process.env.PORT || '3000'
 
 app.set('port', port)
