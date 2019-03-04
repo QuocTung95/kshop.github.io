f4853b5db76f2a1edce2308bfc01159e221ae42f
QuocTung95 <quoctung>
2019-03-04 10:02:20 +0700

    make it better

---
+++ node/migrations/create_user.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      age : {
+        type: Sequelize.STRING
+      },
+      email : {
+        type: Sequelize.STRING
+      },
+      phone : {
+        type: Sequelize.STRING
+      },
+      password : {
+        type: Sequelize.STRING
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
